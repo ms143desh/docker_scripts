f4d0b6550e39892cd2ceff3cdfb6d77915b4452c
Desh Aggarwal <deshbandhu.aggarwal@peerislands.io>
2022-06-15 11:35:29 -0500

    Added localhost:container port mapping for target cluster mongodb508

--- mongodb508_docker/target_start_docker_mongodb508_script.sh
+++ mongodb508_docker/target_start_docker_mongodb508_script.sh
@@ -16,31 +16,31 @@ sleep 10
 
 # ADD TWO SHARDS (mongors1, and mongors2)
 echo "MongoShell - Add shard mongors2n1"
-docker exec -it mongostarget1 bash -c "echo 'sh.addShard(\"mongotargetrs1/mongotargetrs1n1:27018,mongotargetrs1n2:27018,mongotargetrs1n3:27018\")' | mongo"
+docker exec -it mongostarget1 bash -c "echo 'sh.addShard(\"mongotargetrs1/mongotargetrs1n1:27018,mongotargetrs1n2:27018,mongotargetrs1n3:27018\")' | mongo --host mongotargetrs1n1:27018"
 sleep 10
 
 
 
 
 echo "MongoShell - run command - rs.status()"
-docker exec -it mongostarget1 bash -c "echo 'rs.status()' | mongo"
+docker exec -it mongostarget1 bash -c "echo 'rs.status()' | mongo --host mongotargetrs1n1:27018"
 sleep 10
 
 echo "MongoShell - run command - db.adminCommand({listShards:1})"
-docker exec -it mongostarget1 bash -c "echo 'db.adminCommand({listShards:1})' | mongo"
+docker exec -it mongostarget1 bash -c "echo 'db.adminCommand({listShards:1})' | mongo --host mongotargetrs1n1:27018"
 sleep 10
 
 echo "MongoShell - run command - db.printShardingStatus()"
-docker exec -it mongostarget1 bash -c "echo 'db.printShardingStatus()' | mongo"
+docker exec -it mongostarget1 bash -c "echo 'db.printShardingStatus()' | mongo --host mongotargetrs1n1:27018"
 sleep 10
 
 echo "MongoShell - run command - db.hostInfo()"
-docker exec -it mongostarget1 bash -c "echo 'db.hostInfo()' | mongo"
+docker exec -it mongostarget1 bash -c "echo 'db.hostInfo()' | mongo --host mongotargetrs1n1:27018"
 sleep 10
 
 echo "MongoShell - run command - db.stats()"
-docker exec -it mongostarget1 bash -c "echo 'db.stats()' | mongo"
+docker exec -it mongostarget1 bash -c "echo 'db.stats()' | mongo --host mongotargetrs1n1:27018"
 sleep 10
 
 echo "MongoShell - run command - db.serverStatus()"
-docker exec -it mongostarget1 bash -c "echo 'db.serverStatus()' | mongo"
+docker exec -it mongostarget1 bash -c "echo 'db.serverStatus()' | mongo --host mongotargetrs1n1:27018"
